5347a3930256aa46fd3495e1f5b924e6003405b5
Klortiz <klortiz@ucsd.edu>
2023-02-01 16:46:23 -0800

    uwu

--- DocSearchServer.java
+++ DocSearchServer.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
- 
+
 class FileHelpers {
     static List<File> getFiles(Path start) throws IOException {
         File f = start.toFile();
